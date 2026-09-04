183bb055c969c82c5a8394aa4c5bfe96b6f30887
Abhishek-69889 <abhit0396@gmail.com>
2026-09-04 12:48:25 +0530

    Understand basic Idea and implement count

--- 05-useState/src/App.jsx
+++ 05-useState/src/App.jsx
@@ -1,11 +1,16 @@
-
+import { useState } from "react"
 
 function App() {
-  
+  const [num, setNum] = useState(10);
+
+  function changeNum(){
+    setNum(20);
+  }
 
   return (
     <>
-      
+      <h1>Value of a is {num}</h1>
+      <button onClick={changeNum}>Click</button>
     </>
   )
 }
