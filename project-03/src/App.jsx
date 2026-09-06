3041ffafe40d6fa09393df3100df029df534d506
Abhishek-69889 <abhit0396@gmail.com>
2026-09-06 20:54:58 +0530

    Complete note app project

--- project-03/src/App.jsx
+++ project-03/src/App.jsx
@@ -5,14 +5,29 @@ import Notes from "./components/Notes"
 
 function App() {
     const [notes, setNotes] = useState([])
+    // console.log(notes)
 
   return (
     <>
      <div className="w-full h-screen flex  justify-between px-5 py-1">
       <Form setnotes={setNotes}/>
       <div className="w-1 h-full bg-amber-50"></div>
-      <div className='w-4/5 h-full overflow-y-auto flex flex-wrap p-1 gap-1'>
-      <Notes note={notes}/>
+      <div className="w-4/5 h-full overflow-y-auto px-1">
+        <h2 className="text-2xl font-bold">Recent notes</h2>
+        <div className=' flex flex-wrap px-5 py-2 gap-1'>
+
+      {/* <Notes notes={notes}/> */}
+      {
+  notes.map((note, index) => (
+    <Notes
+      key={index}
+      idx={index}
+      title={note.title}
+      description={note.description}
+    />
+  ))
+}
+      </div>
       </div>
      </div>
     </>
--- project-03/src/components/Notes.jsx
+++ project-03/src/components/Notes.jsx
@@ -1,27 +1,22 @@
 import React from 'react'
 
 const Notes = (props) => {
-  console.log(props.note[0])
+  console.log(props)
+  
   return (
         <div className="w-60 h-60 overflow-hidden  rounded-2xl border-2 border-red-400 border-solid">
           <div className=" relative object-cover ">
             {/* <img src="https://img.magnific.com/free-psd/vintage-spiral-notebook-blank-pages-your-ideas_191095-83874.jpg?semt=ais_hybrid&w=740&q=80" alt=""  className="object-cover"/> */}
           </div>
-          {
-            props.note.map((item,index)=>{
-              return (
-              <div className='h-full flex justify-between flex-col p-2' index={index}>
-            <div className="text-white flex flex-col gap-10">
-              <h2 className='text-xl font-bold'>{item.title}</h2>
-              <p className='text-sm text-gray-600 font-medium'>{item.description}</p>
+          <div className='h-full flex justify-between flex-col p-2' >
+            <div className="text-white flex flex-col gap-3">
+              <h2 className='text-xl font-bold'>{props.title}</h2>
+              <p className='text-sm text-gray-600 font-medium'>{props.description}</p>
             </div>
             <div className= ''>
               <button className='bg-red-500 text-white p-1 px-2 rounded-2xl cursor-pointer'>Delete</button>
             </div>
           </div>
-              )
-            })
-          }
         </div>    
   )
 }
